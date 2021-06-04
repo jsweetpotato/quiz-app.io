a6d40728db069d2d6630b2d1bd2c503e79803959
jisu kim <lerry37@naver.com>
2021-06-05 03:27:14 +0900

    finish

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const quizData = [
         d: '110',
         correct: 'c'
     }, {
-        question: 'What is the most used programing language? in 2021?',
+        question: 'What is the most used programing language in 2021?',
         a: 'Java',
         b: 'C',
         c: 'Python',
@@ -22,3 +22,72 @@ const quizData = [
         correct: 'a'
     }
 ];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+
+    a_text.innerText = currentQuizData,question;
+
+    a_text.innerText = currentQuizData.a;   
+    b_text.innerText = currentQuizData.b;   
+    c_text.innerText = currentQuizData.c;   
+    d_text.innerText = currentQuizData.d;   
+
+}
+
+function getSelected(){
+    
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer =  answerEl.id;
+        }        
+    });
+
+    return answer;
+}
+
+function deselectAnswers(){
+
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    
+    console.log(answer);
+
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++   
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        }else{
+            quiz.innerHTML = `<h2>Your answered correctly at ${score} / ${quizData.length} questions.</h2> <button onclick="location.reload()">Reload</button>`
+        }
+    }
+
+})
